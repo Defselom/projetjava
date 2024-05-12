b617fb3639ebd7b630b57bb0529c84452a598d80
m2pro <selomadzomla@gmail.com>
2024-05-12 06:48:33 +0000

    test methode

--- src/selom/Ppoo_sadzomla.java
+++ src/selom/Ppoo_sadzomla.java
@@ -5,6 +5,8 @@
 package selom;
 
 import java.util.logging.Logger;
+import selom.entities.Client;
+import selom.services.implementations.ClientServiceBean;
 
 /**
  *
@@ -21,5 +23,31 @@ public class Ppoo_sadzomla {
         // Call info method 
         logger.info("Message 1"); 
         logger.info("Message 2"); 
+        
+        Client def = new Client();
+        def.setNom("defselom");
+        def.setPrenom("elolo");
+        def.setTelephone("99780518");
+        
+        
+       // Assuming you have instantiated ClientServiceBean somewhere in your application context
+ClientServiceBean clientService = new ClientServiceBean();
+
+// Create a new client object
+Client client = new Client();
+client.setNom("bezos ");
+client.setPrenom("jeff");
+client.setTelephone("90959090");
+// Set other properties of the client as needed
+
+try {
+    // Call the save method to save the client
+    clientService.save2(client);
+    System.out.println("Client saved successfully!");
+} catch (Exception e) {
+    // Handle any exceptions that may occur during the saving process
+    System.err.println("Error saving client: " + e.getMessage());
+}
+
     } 
 }
