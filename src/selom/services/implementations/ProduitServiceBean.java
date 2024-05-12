47cff69c9399c1364f649261df19cd74616e5b32
m2pro <selomadzomla@gmail.com>
2024-05-12 17:15:30 +0000

    add Product controller

---
+++ src/selom/services/implementations/ProduitServiceBean.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package selom.services.implementations;
+
+import java.util.List;
+import selom.entities.Produit;
+import selom.services.ProduitServiceBeanLocal;
+
+/**
+ *
+ * @author m2pro
+ */
+public class ProduitServiceBean implements ProduitServiceBeanLocal {
+
+    @Override
+    public List<Produit> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void ajouter() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void modifier() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void supprimer() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
