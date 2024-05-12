5382187d603a9ab5d740d9357a96229e5125ef74
m2pro <selomadzomla@gmail.com>
2024-05-12 19:24:05 +0000

    feat : update service to get and save the client id

--- src/selom/services/implementations/ClientServiceBean.java
+++ src/selom/services/implementations/ClientServiceBean.java
@@ -18,28 +18,32 @@ import selom.utils.ConnectDB;
  *
  * @author m2pro
  */
-public class ClientServiceBean implements ClientServiceBeanLocal  {
-    
-    
+public class ClientServiceBean implements ClientServiceBeanLocal {
+
     private final ConnectDB cn;
 
     public ClientServiceBean() {
         this.cn = new ConnectDB();
     }
-    
-  
-     public void save(Client client) {
+
+    public void save(Client client) {
         try {
             //
             cn.makeConnection();
             //
             String RequeteAjout = "INSERT INTO `client`(`nom`,`prenom`,`telephone`) "
                     + "VALUES (?,?,?)";
-            PreparedStatement PreparedStmt = cn.makeConnection().prepareStatement(RequeteAjout);
+            PreparedStatement PreparedStmt = cn.makeConnection().prepareStatement(RequeteAjout,Statement.RETURN_GENERATED_KEYS);
             PreparedStmt.setString(1, client.getNom());
             PreparedStmt.setString(2, client.getPrenom());
             PreparedStmt.setString(3, client.getTelephone());
             PreparedStmt.executeUpdate();
+
+            ResultSet res = PreparedStmt.getGeneratedKeys();
+            while (res.next()) {
+                int userId = res.getInt(1);
+                client.setId(userId);
+            }
         } catch (SQLException ex) {
             Logger.getLogger(ClientServiceBean.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -51,15 +55,15 @@ public class ClientServiceBean implements ClientServiceBeanLocal  {
     }
 
     @Override
-    public Client findById(int k) {
+    public Client findById(int k
+    ) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
-    public void deleteById(int k) {
+    public void deleteById(int k
+    ) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
-   
 
 }
