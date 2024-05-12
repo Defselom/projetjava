1cef3dbca51e606e495f2dffd95078a79d609b45
m2pro <selomadzomla@gmail.com>
2024-05-12 06:48:20 +0000

    update ClientServiceBean

--- src/selom/services/implementations/ClientServiceBean.java
+++ src/selom/services/implementations/ClientServiceBean.java
@@ -27,63 +27,7 @@ public class ClientServiceBean implements ClientServiceBeanLocal  {
         this.cn = new ConnectDB();
     }
     
-       @Override
-    public void save(Client client) {
-        PreparedStatement ps=null;
-         Connection connexion= ConnectBD.seConnecter();
-        if(!VerifierDoublonClient(client.getNom())){
-            String InsertSql="insert into Client(idClient,nom,prenom,telephone) values(?,?,?,?)";
-           
-            try {
-                ps=connexion.prepareStatement(InsertSql);
-                ps.setInt(1,client.getIdClient());
-                ps.setString(2,client.getNom());
-                ps.setString(3,client.getPrenom());
-                ps.setString(4,client.getTelephone());
-                ps.executeUpdate();
-            } catch (SQLException ex) {
-                java.util.logging.Logger.getLogger(ClientServicesImpl.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-            }
-            
-            
-        }else{
-            String req="update client set nom=? , prenom=? , telephone=? where idClient=(select idClient from client where nom=?)";
-                Scanner sc= new Scanner(System.in);
-            try {
-                ps=connexion.prepareStatement(req);
-                 System.out.println(" un nouveau nom sous peu pour modifier le client!");
-                ps.setString(1, sc.nextLine());
-                System.out.println("un nouveau preom");
-                ps.setString(2, sc.nextLine());
-                System.out.println("un nouveau telephone");
-                ps.setString(3, sc.nextLine());
-                ps.setString(4, client.getNom());
-                ps.executeUpdate();
-                
-            } catch (SQLException ex) {
-                java.util.logging.Logger.getLogger(ClientServicesImpl.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-            }
-             
-        }
-        if(connexion!=null){
-                try {
-                    connexion.close();
-                } catch (SQLException ex) {
-                    java.util.logging.Logger.getLogger(ClientServicesImpl.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-                }
-            }
-            if(ps!=null){
-                try {
-                    ps.close();
-                } catch (SQLException ex) {
-                    java.util.logging.Logger.getLogger(ClientServicesImpl.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-                }
-            }
-           
-        
-    }
-
-
+  
      public void save2(Client client) {
         try {
             //
@@ -113,6 +57,11 @@ public class ClientServiceBean implements ClientServiceBeanLocal  {
 
     @Override
     public void deleteById(int k) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void save(Client t) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
