38acbca6668584ee0fa77a86ee47306bf89dfc7b
m2pro <selomadzomla@gmail.com>
2024-05-13 08:30:00 +0000

    chore : test

--- src/selom/Ppoo_sadzomla.java
+++ src/selom/Ppoo_sadzomla.java
@@ -39,54 +39,94 @@ public class Ppoo_sadzomla {
         SouscriptionController souscriptionController = new SouscriptionController(maSouscriptionService);
 
         // Create a Logger with class name GFG 
-        Logger logger
-                = Logger.getLogger(Ppoo_sadzomla.class.getName());
-
+        // Logger logger= Logger.getLogger(Ppoo_sadzomla.class.getName());
         // Call info method 
-        logger.info("Message 1");
-        logger.info("Message 2");
-
+//        logger.info("Message 1");
+//        logger.info("Message 2");
         // create client
-        Client def = new Client();
-        def.setNom("defselom");
-        def.setPrenom("elolo");
-        def.setTelephone("99780518");
-
-        // create produit
-        Produit cafe = new Produit();
-        cafe.setLibelle("Coca cola");
-        cafe.setActif("T");
-
-        // create sms
-        Sms mySms = new Sms();
-        mySms.setIdClient(def);
-        mySms.setLibelle("Merci pour votre achat");
-        mySms.setStatus("en Attente");
-
-        // create souscription
-        Souscription mySouscription = new Souscription();
-        mySouscription.setIdClient(def);
-        mySouscription.setIdProduit(cafe);
-        mySouscription.setActif("T");
-
-       
-       
-// Set other properties of the client as needed
+//        Client def = new Client();
+//        def.setNom("defselom");
+//        def.setPrenom("elolo");
+//        def.setTelephone("99780518");
+//
+//        // create produit
+//        Produit cafe = new Produit();
+//        cafe.setLibelle("Coca cola");
+//        cafe.setActif("T");
+//
+//        // create sms
+//        Sms mySms = new Sms();
+//        mySms.setIdClient(def);
+//        mySms.setLibelle("Merci pour votre achat");
+//        mySms.setStatus("en Attente");
+//
+//        // create souscription
+//        Souscription mySouscription = new Souscription();
+//        mySouscription.setIdClient(def);
+//        mySouscription.setIdProduit(cafe);
+//        mySouscription.setActif("T");
+//// Set other properties of the client as needed
+//        try {
+//            // Call the save method to save the client
+//            clientController.saveClient(def);
+//            //  produitController.saveProduit(cafe);
+//            //  souscriptionController.save(mySouscription);
+//            //  smsController.save(mySms);
+//            //  clientController.getAllClient();
+//            //  clientController.getById(2);
+//            //clientController.deleteById(2);
+//            def.setId(2098);
+//            clientController.update(def);
+//        } catch (Exception e) {
+//            // Handle any exceptions that may occur during the saving process
+//            System.err.println("Error saving : " + e.getMessage());
+//        }
+//
+//    }
+// Manipulation des classes
         try {
-            // Call the save method to save the client
-            clientController.saveClient(def);
-          //  produitController.saveProduit(cafe);
-          //  souscriptionController.save(mySouscription);
-          //  smsController.save(mySms);
-          //  clientController.getAllClient();
-          //  clientController.getById(2);
-             //clientController.deleteById(2);
-             def.setId(2098);
-clientController.update(def);
+            //Avec le contrôleur du Client, ajouter quatre (04) clients.
+            Client Justin = new Client("NEON", "Justin", "+22890122334");
+            Client Jean = new Client("YEMBOATE", "Jean", "+22890142231");
+            Client Jeanne = new Client("YEMBOATE", "Jeanne", "+22892124689");
+            Client Claude = new Client("SABI", "Claude", "+22894154581");
+
+            clientController.saveClient(Justin);
+            clientController.saveClient(Jean);
+            clientController.saveClient(Jeanne);
+            clientController.saveClient(Claude);
+
+            //Avec le contrôleur Produit, ajouter les produits actifs « Epargne » et « Courant ».
+            Produit produitEpargne = new Produit("Epargne", "T");
+            Produit produitCourant = new Produit("Courant", "T");
+
+            produitController.saveProduit(produitEpargne);
+            produitController.saveProduit(produitCourant);
+
+            //Avec le contrôleur de souscription, ajouter pour les produits « Epargne et Courant »
+            //pour un client de votre choix et le produit « Courant » pour un autre client de votre
+            //choix.
+            Souscription justinCourantSouscription = new Souscription("T", Justin, produitCourant);
+            
+            Souscription justinEpargneSouscription = new Souscription("T", Justin, produitEpargne);
+
+            Souscription jeanneCourantSouscription = new Souscription("T", Jeanne, produitCourant);
+
+            souscriptionController.save(justinCourantSouscription);
+            souscriptionController.save(justinEpargneSouscription);
+            souscriptionController.save(jeanneCourantSouscription);
+
+            //  produitController.saveProduit(cafe);
+            //  souscriptionController.save(mySouscription);
+            //  smsController.save(mySms);
+            //  clientController.getAllClient();
+            //  clientController.getById(2);
+            //clientController.deleteById(2);
+//            def.setId(2098);
+//            clientController.update(def);
         } catch (Exception e) {
             // Handle any exceptions that may occur during the saving process
             System.err.println("Error saving : " + e.getMessage());
         }
-
     }
 }
